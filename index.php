45f9271012b3ae07c2422369cdbf81809fd01a18
doconno6 <doconno6@uvm.edu>
2019-10-26 12:40:58 -0400

    Basic homepage, sort of

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="custom.css" rel="stylesheet">
+    <title>Home Page</title>
+</head>
+<body>
+
+<nav class="navbar fixed-top navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">
+        <img src="https://pbs.twimg.com/profile_images/935211463232376832/DC7I1vwd_400x400.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Home
+    </a>
+    <a class="navbar-brand" href="account.html">Account</a>
+    <a class="navbar-brand" href="#">About</a>
+    <a class="navbar-brand" href="#">Register</a>
+</nav>
+
+<br>
+<div class="container-fluid" STYLE="margin: 100px">
+    <div class="card">
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+    <div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+    <div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div><div class="card" >
+    <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+    <div class="card-body">
+        <h3 class="card-title">Card title</h3>
+        <h4 class="card-subtitle">Card subtitle</h4>
+        <p class="card-text">This is a simple Card example</p>
+    </div>
+    </div><div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div><div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div><div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+    <div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+    <div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+    <div class="card" >
+        <img class="card-img-top" src="https://images.rapgenius.com/d675dba75d068e69e82f339d5d085dfa.941x522x1.png" alt="Card image top">
+        <div class="card-body">
+            <h3 class="card-title">Card title</h3>
+            <h4 class="card-subtitle">Card subtitle</h4>
+            <p class="card-text">This is a simple Card example</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
