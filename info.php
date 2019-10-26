bbba3ea2f7fc48648ee065c452b14d7554b9e65d
doconno6 <doconno6@uvm.edu>
2019-10-26 16:11:02 -0400

    Basic homepage, sort of

--- info.php
+++ info.php
@@ -1,84 +1,83 @@
 <?php include 'header.php'?>
-  <p> . </p>
-  <p> . </p>
-  <div class="container">
+
+<br><br>
+<div class="container">
     <h2> Project Description </h2>
-  </div>
-  <p> . </p>
-  <img src="contact.png" alt="contact page" width="600" class="full-block">
+</div>
+<br>
+<img src="contact.png" alt="contact page" width="600" class="full-block">
 
-  <div class="caption">
-    <p> project information + github repo (trello board) </p>
-  </div>
+<div class="caption">
+    <p style="alignment: center"> project information + github repo (trello board) </p>
+</div>
 
 
-  <div class="container">
-    <p> . </p>
-    <p> . </p>
+<div class="container">
+    <br><br>
     <h2>Web Devs</h2>
-    <p> . </p>
+    <br>
 
     <div class="row">
 
-      <div class="col-md-6">
-        <div class="thumbnail">
-          <img src="pp.png" alt="Clasby pp" style="width:100%">
-          <div class="caption">
-            <p>CLASBY</p>
-            <p>CS Major + focus</p>
-            <p>Back End</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6">
-        <div class="thumbnail">
-          <img src="pp.png" alt="Prasidha pp" style="width:100%">
-          <div class="caption">
-            <p>PRASIDHA</p>
-            <p>CS Major + focus</p>
-            <p>Back End</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6">
-        <div class="thumbnail">
-          <img src="pp.png" alt="Dan pp" style="width:100%" >
-          <div class="caption">
-            <p>DAN D.</p>
-            <p>CS Major + focus</p>
-            <p>Front End</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6">
-        <div class="thumbnail">
-          <a href="pp.png" target="www.youtube.com">
-            <img src="pp.png" alt="Matheus pp" style="width:100%" class="center">
-            <div class="caption">
-              <p>MATHEUS</p>
-              <p>ME Major + CS Minor</p>
-              <p>Front End</p>
+        <div class="col-md-6">
+            <div class="thumbnail">
+                <img src="pp.png" alt="Clasby pp" style="width:100%">
+                <div class="caption">
+                    <p>CLASBY</p>
+                    <p>CS Major + focus</p>
+                    <p>Back End</p>
+                </div>
             </div>
-          </a>
         </div>
-      </div>
+
+        <div class="col-md-6">
+            <div class="thumbnail">
+                <img src="pp.png" alt="Prasidha pp" style="width:100%">
+                <div class="caption">
+                    <p>PRASIDHA</p>
+                    <p>CS Major + focus</p>
+                    <p>Back End</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="thumbnail">
+                <img src="pp.png" alt="Dan pp" style="width:100%" >
+                <div class="caption">
+                    <p>DAN D.</p>
+                    <p>CS Major + focus</p>
+                    <p>Front End</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="thumbnail">
+                <a href="pp.png" target="www.youtube.com">
+                    <img src="pp.png" alt="Matheus pp" style="width:100%" class="center">
+                    <div class="caption">
+                        <p>MATHEUS</p>
+                        <p>ME Major + CS Minor</p>
+                        <p>Front End</p>
+                    </div>
+                </a>
+            </div>
+        </div>
 
     </div>
-  </div>
+</div>
 
-  <br>
-  <div class="container">
+<br>
+<div class="container">
     <h2> Scrum: Sprint Activities </h2>
-  </div>
-  <img class="img-responsive" src="contact.png" alt="contact page" width="460" height="345">
-  <div class="caption">
+</div>
+<img class="full-block" src="contact.png" alt="contact page" width="460" height="345" ">
+<div class="caption">
     <p> sprint 1 + sprint 2 details (trello board) </p>
-  </div>
-  <br>
+</div>
+<br>
 
-  </body>
-  </html>
+</body>
+</html>
 
