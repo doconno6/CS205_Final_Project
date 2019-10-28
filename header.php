a0124f28413fa37457c31d44daf26cc6ce4c528f
doconno6 <doconno6@uvm.edu>
2019-10-28 19:25:06 -0400

    nav changes

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
         <img src="https://pbs.twimg.com/profile_images/935211463232376832/DC7I1vwd_400x400.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
         Home
     </a>
-    <div class="mx-auto">
+    <div class="mx-2">
         <a class="navbar-brand" href="account.php">Account </a>
         <a class="navbar-brand" href="info.php">Info </a>
         <a class="navbar-brand" href="">Register </a>
