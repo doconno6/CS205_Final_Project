370f42a2e7599bf3148bff503bede84ff8d86c6e
doconno6 <doconno6@uvm.edu>
2019-10-28 19:24:29 -0400

    nav changes

--- header.php
+++ header.php
@@ -18,9 +18,11 @@
         <img src="https://pbs.twimg.com/profile_images/935211463232376832/DC7I1vwd_400x400.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
         Home
     </a>
-    <a class="navbar-brand" href="account.php">Account </a>
-    <a class="navbar-brand" href="info.php">Info </a>
-    <a class="navbar-brand" href="">Register </a>
+    <div class="mx-auto">
+        <a class="navbar-brand" href="account.php">Account </a>
+        <a class="navbar-brand" href="info.php">Info </a>
+        <a class="navbar-brand" href="">Register </a>
+    </div>
 
     <form class="form-inline">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
